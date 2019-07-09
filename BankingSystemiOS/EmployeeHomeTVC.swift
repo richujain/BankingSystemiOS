086213dca0b9efd0acbbb055c3c0d4527f752c0d
Richu Jain <richujain1995@gmail.com>
2019-07-09 02:42:08 -0400

    Added some minor changes.

--- BankingSystemiOS/EmployeeHomeTVC.swift
+++ BankingSystemiOS/EmployeeHomeTVC.swift
@@ -14,6 +14,7 @@ class EmployeeHomeTVC: UITableViewController {
     @IBOutlet var employeeActivities: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController!.navigationBar.isHidden = true;
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
