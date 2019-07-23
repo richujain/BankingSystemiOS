dc2cae548d352a9e7c1797c530f287a78c59a6c5
ashishpratheesh <ashishpradeesh@gmail.com>
2019-07-22 20:39:46 -0400

    All attributes for viewing user details declared.

--- BankingSystemiOS/ViewUserViewController.swift
+++ BankingSystemiOS/ViewUserViewController.swift
@@ -7,16 +7,31 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewUserViewController: UIViewController {
 
+    @IBOutlet weak var txtAccountNumber: UITextField!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelAddress: UILabel!
+    @IBOutlet weak var labelBirthDate: UILabel!
+    @IBOutlet weak var labelEmailID: UILabel!
+    @IBOutlet weak var labelIdProofNumber: UILabel!
+    @IBOutlet weak var labelBalance: UILabel!
+    @IBOutlet weak var labelBankBranch: UILabel!
+    @IBOutlet weak var btnDone: UIButton!
+    @IBOutlet weak var labelContactNumber: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        btnDone.isHidden = true
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func btnFetch(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnDone(_ sender: UIButton) {
+    }
     /*
     // MARK: - Navigation
 
